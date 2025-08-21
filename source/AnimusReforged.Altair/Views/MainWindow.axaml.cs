3814f54a8acf7d6ec2a96666786d7dc4e5ee9442
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-08-21 14:10:43 +0200

    Added logging

--- source/AnimusReforged.Altair/Views/MainWindow.axaml.cs
+++ source/AnimusReforged.Altair/Views/MainWindow.axaml.cs
@@ -12,16 +12,20 @@ public partial class MainWindow : AppWindow
     public MainWindow()
     {
         InitializeComponent();
+#if !DEBUG
         Loaded += async (_, _) =>
         {
+            Logger.Info("Checking if the AnimusReforged has been placed next to the game executable");
             if (!File.Exists(AppPaths.GameExecutable))
             {
-                await MessageBox.ShowAsync("Game executable not found. Please make sure you have the game installed and the executable is in the correct location.", "Error", App.MainWindow);
+                Logger.Error("Missing game executable, terminating");
+                await MessageBox.ShowAsync($"Game executable not found. Please make sure you have the game installed and the executable is in the game folder next to the {AppPaths.GameExecutable}.", "Error", App.MainWindow);
                 Environment.Exit(0);
             }
         };
+#endif
     }
-    
+
     private void NavView_OnSelectionChanged(object? sender, NavigationViewSelectionChangedEventArgs e)
     {
         if (DataContext is MainWindowViewModel vm && e.SelectedItem is NavigationViewItem nvi)
