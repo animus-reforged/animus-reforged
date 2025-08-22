4f1822a7fd1e0fc6188e3555fe4c1a71bd69b5fa
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-08-22 14:34:24 +0200

    Added Credits Page

--- source/AnimusReforged.Altair/ViewModels/Pages/CreditsPageViewModel.cs
+++ source/AnimusReforged.Altair/ViewModels/Pages/CreditsPageViewModel.cs
@@ -1,6 +1,100 @@
-﻿namespace AnimusReforged.Altair.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.Input;
+using FluentIcons.Common;
 
-public class CreditsPageViewModel : ViewModelBase
+namespace AnimusReforged.Altair.ViewModels;
+
+public partial class CreditsPageViewModel : ViewModelBase
 {
-    
+    public class CreditItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public Symbol Icon { get; set; } = Symbol.Link;
+    }
+
+    public ObservableCollection<CreditItem> Credits { get; }
+
+    public CreditsPageViewModel()
+    {
+        Credits = new ObservableCollection<CreditItem>
+        {
+            new CreditItem
+            {
+                Title = "uMod Tool",
+                Description = "For allowing texture modifications",
+                Url = "https://code.google.com/archive/p/texmod/",
+                Icon = Symbol.Wrench
+            },
+            new CreditItem
+            {
+                Title = "hecumarine",
+                Description = "For creating Overhaul Mod",
+                Url = "https://www.moddb.com/mods/assassins-creed-2014-overhaul",
+                Icon = Symbol.Sparkle
+            },
+            new CreditItem
+            {
+                Title = "Sergeanur",
+                Description = "For creating EaglePatch",
+                Url = "https://github.com/Sergeanur/EaglePatch",
+                Icon = Symbol.Code
+            },
+            new CreditItem
+            {
+                Title = "crosire",
+                Description = "For creating ReShade",
+                Url = "https://reshade.me/",
+                Icon = Symbol.ColorBackground
+            },
+            new CreditItem
+            {
+                Title = "Ciocolici",
+                Description = "For the ReShade Preset",
+                Url = "https://steamcommunity.com/sharedfiles/filedetails/?id=2957930769",
+                Icon = Symbol.PaintBrush
+            },
+            new CreditItem
+            {
+                Title = "sony33d",
+                Description = "For icon used in Discord Rich Presence",
+                Url = "https://www.deviantart.com/sony33d/art/Assassin-s-Creed-Icon-Pack-PNG-ICO-555468864",
+                Icon = Symbol.Emoji
+            },
+            new CreditItem
+            {
+                Title = "00031",
+                Description = "For creating banner and upscaling background image",
+                Url = "https://www.nexusmods.com/users/70787823",
+                Icon = Symbol.ImageSparkle
+            }
+        };
+    }
+
+    [RelayCommand]
+    private void OpenLink(string url)
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            Logger.Warning("No URL provided to open");
+            return;
+        }
+
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            Logger.Error($"There was an error opening the link: {url}");
+            Logger.LogExceptionDetails(ex);
+        }
+    }
 }
