a4069d44bda900564922d628986fd9eeb5184b65
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-09-17 11:28:24 +0200

    Finished UI for ManagePage

--- source/AnimusReforged.Altair/ViewModels/Pages/ManagePageViewModel.cs
+++ source/AnimusReforged.Altair/ViewModels/Pages/ManagePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Threading.Tasks;
 using AnimusReforged.Altair.Views;
 using AnimusReforged.Mods.Altair;
@@ -17,7 +18,7 @@ public partial class ManagePageViewModel : ViewModelBase
     // Variables
     // Download Progress Bar
     [ObservableProperty] private bool isDownloading;
-    [ObservableProperty] private string downloadStatus = string.Empty;
+    [ObservableProperty] private string downloadStatus = "Waiting for download...";
     [ObservableProperty] private int downloadProgress;
 
     // Mods
@@ -56,44 +57,63 @@ public partial class ManagePageViewModel : ViewModelBase
             switch (SelectedMod)
             {
                 case "Ultimate ASI Loader":
+                    Logger.Info("Downloading Ultimate ASI Loader");
                     DownloadStatus = "Downloading Ultimate ASI Loader";
                     await ModManager.DownloadAsiLoader(progress => DownloadProgress = progress);
+                    Logger.Info("Installing Ultimate ASI Loader");
                     DownloadStatus = "Installing Ultimate ASI Loader";
                     ModManager.InstallAsiLoader();
                     break;
                 case "AltairFix":
+                    Logger.Info("Downloading AltairFix");
                     DownloadStatus = "Downloading AltairFix";
                     await ModManager.DownloadAltairFix(progress => DownloadProgress = progress);
+                    Logger.Info("Installing AltairFix");
                     DownloadStatus = "Installing AltairFix";
                     ModManager.InstallAltairFix();
                     break;
                 case "EaglePatch":
+                    Logger.Info("Downloading EaglePatch mod");
                     DownloadStatus = "Downloading EaglePatch mod";
                     await ModManager.DownloadEaglePatch(progress => DownloadProgress = progress);
+                    Logger.Info("Installing EaglePatch mod");
                     DownloadStatus = "Installing EaglePatch mod";
                     ModManager.InstallEaglePatch();
                     break;
                 case "uMod":
+                    Logger.Info("Downloading uMod");
                     DownloadStatus = "Downloading uMod";
                     await ModManager.DownloaduMod(progress => DownloadProgress = progress);
+                    Logger.Info("Installing uMod");
                     DownloadStatus = "Installing uMod";
                     ModManager.InstalluMod();
                     break;
                 case "Overhaul":
+                    Logger.Info("Downloading Overhaul mod");
                     DownloadStatus = "Downloading Overhaul mod";
                     await ModManager.DownloadOverhaul(progress => DownloadProgress = progress);
+                    Logger.Info("Installing Overhaul mod");
                     DownloadStatus = "Installing Overhaul mod";
                     ModManager.InstallOverhaul();
                     break;
                 case "ReShade":
+                    Logger.Info("Downloading ReShade");
                     DownloadStatus = "Downloading ReShade";
                     await ModManager.DownloadReShade(progress => DownloadProgress = progress);
+                    Logger.Info("Installing ReShade");
                     DownloadStatus = "Installing Reshade";
                     ModManager.InstallReShade();
                     App.Settings.Tweaks.Reshade = true;
                     App.AppSettings.SaveSettings();
                     break;
             }
+            if (Directory.Exists(AppPaths.Downloads))
+            {
+                Logger.Info("Deleting downloads");
+                Directory.Delete(AppPaths.Downloads, true);
+            }
+            Logger.Info("Downloading complete.");
+            await MessageBox.ShowAsync("Download Complete.", MessageBoxButtons.Ok);
         }
         catch (Exception ex)
         {
