5f2371140fd03e97325b303aa5d1478eea7365f4
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-01-04 09:38:13 +0100

    feat(logger): Project `Logger` Class

--- source/AnimusReforged.Altair/App.axaml.cs
+++ source/AnimusReforged.Altair/App.axaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using AnimusReforged.Altair.Services;
 using Avalonia;
 using Avalonia.Controls;
@@ -7,6 +8,8 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using AnimusReforged.Altair.Views;
+using AnimusReforged.Logging;
+using Avalonia.Threading;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AnimusReforged.Altair;
@@ -18,21 +21,23 @@ public partial class App : Application
     /// Desktop instance
     /// </summary>
     public static IClassicDesktopStyleApplicationLifetime? Desktop = Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
-    
+
     /// <summary>
     /// Main Window instance
     /// </summary>
     public static Window? MainWindow => Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ? desktop.MainWindow : null;
-    
+
     /// <summary>
     /// DI Services
     /// </summary>
     public static IServiceProvider Services { get; private set; } = null!;
-    
+
     // Functions
     public override void Initialize()
     {
+        Logger.Debug<App>("Initializing Avalonia application");
         AvaloniaXamlLoader.Load(this);
+        Logger.Debug<App>("Avalonia XAML loaded successfully");
     }
 
     public override void OnFrameworkInitializationCompleted()
@@ -40,23 +45,111 @@ public partial class App : Application
         if (Desktop is { } desktop)
         {
             // Disable Avalonia validation
+            Logger.Trace<App>("Disabling Avalonia data annotation validation");
             DisableAvaloniaDataAnnotationValidation();
-            
+
             // Configure services
+            Logger.Debug<App>("Configuring dependency injection services");
             try
             {
                 Services = ServiceConfigurator.ConfigureServices();
+                Logger.Info<App>("Services configured successfully");
             }
             catch (Exception ex)
             {
+                Logger.Error<App>("Failed to configure services");
+                Logger.LogExceptionDetails<App>(ex);
                 throw;
             }
-            
+
+            // Get MainWindow
+            Logger.Debug<App>("Resolving MainWindow from services");
             MainWindow mainWindow = Services.GetRequiredService<MainWindow>();
             desktop.MainWindow = mainWindow;
+
+            // Wire up window events
+            mainWindow.Opened += (_, _) =>
+            {
+                Logger.Info<App>("Launching AnimusReforged Altair");
+                Logger.Info<App>($"Version: ");
+                Logger.Debug<App>("Main window opened");
+            };
+
+            mainWindow.Closing += (_, e) =>
+            {
+                Logger.Info<App>("Main window closing");
+                Logger.Debug<App>("Flushing logs before shutdown");
+                Logger.Flush();
+            };
+
+            // Application exit handler
+            desktop.Exit += (_, _) =>
+            {
+                Logger.Info<App>("Closing AnimusReforged Altair");
+                Logger.Debug<App>("Shutting down logger");
+                Logger.Shutdown();
+            };
+
+            // Global exception handlers
+            Logger.Trace<App>("Registering global exception handlers");
+
+            TaskScheduler.UnobservedTaskException += (_, args) =>
+            {
+                args.SetObserved();
+                Logger.Error<App>("Unobserved task exception occurred");
+                HandleFatalException(args.Exception);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+            {
+                bool isTerminating = args.IsTerminating;
+                Logger.Error<App>($"Unhandled exception in AppDomain (Terminating: {isTerminating})");
+
+                if (args.ExceptionObject is Exception ex)
+                {
+                    HandleFatalException(ex);
+                }
+                else
+                {
+                    Logger.Error<App>($"Non-exception object thrown: {args.ExceptionObject?.GetType().FullName ?? "null"}");
+                }
+            };
+
+            Dispatcher.UIThread.UnhandledException += (_, args) =>
+            {
+                args.Handled = true;
+                Logger.Error<App>("Unhandled exception on UI thread");
+                HandleFatalException(args.Exception);
+            };
+
+            Logger.Debug<App>("Setting main window");
+            desktop.MainWindow = mainWindow;
+
+            Logger.Info<App>("Application initialization completed successfully");
+        }
+        else
+        {
+            Logger.Warning<App>("Application is not running as desktop application");
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void HandleFatalException(Exception ex)
+    {
+        try
+        {
+            Logger.Error<App>("=== Fatal Exception Encountered ===");
+            Logger.LogExceptionDetails<App>(ex, includeEnvironmentInfo: true);
+
+            // Ensure logs are written before potential crash
+            Logger.Flush();
+        }
+        catch
+        {
+            // If logging fails, we can't do much about it
+            // Just ensure we don't throw from the exception handler
+        }
     }
 
     private void DisableAvaloniaDataAnnotationValidation()
